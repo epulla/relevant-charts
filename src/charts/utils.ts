2d5bbfd4369f1af01a052cef440121e03b8e1ff2
epulla <erickpulladsi@gmail.com>
2024-07-22 02:14:11 -0500

    fix: disable `area` graph for now, fix `mapLabelsWithData` and format code

--- src/charts/utils.ts
+++ src/charts/utils.ts
@@ -4,19 +4,6 @@ import { ShadcnLineChart } from "./components/line-chart";
 import { ShadcnPieChart } from "./components/pie-chart";
 import { ChartProps } from "./types";
 
-// export const getProcessedData = (chartResponse: ChartResponse) => {
-//   return chartResponse.labels.map((item, i) => ({
-//     label: item,
-//     ...chartResponse.data.reduce(
-//       (acc, { name, values }) => ({
-//         ...acc,
-//         [name]: values[i] || 0,
-//       }),
-//       {}
-//     ),
-//   }));
-// };
-
 export const SUPPORTED_CHARTS_WITH_STRATEGIES: {
   [id: string]: {
     component: ({ chartResponse }: ChartProps) => JSX.Element;
@@ -29,15 +16,15 @@ export const SUPPORTED_CHARTS_WITH_STRATEGIES: {
   },
   pie: {
     component: ShadcnPieChart,
-    strategies: [],
+    strategies: ["mapLabelsWithData", "groupByLabelsAndCount"],
   },
-  area: {
-    component: ShadcnAreaChart,
-    strategies: [],
-  },
+  // area: {
+  //   component: ShadcnAreaChart,
+  //   strategies: [],
+  // },
   line: {
     component: ShadcnLineChart,
-    strategies: [],
+    strategies: ["mapLabelsWithData", "groupByLabelsAndCount"],
   },
 };
 
@@ -48,17 +35,21 @@ export const SUPPORTED_CHARTS_STRATEGIES: {
   mapLabelsWithData: (data: any[], labelColumn: string, dataColumn: string) => {
     return data.map((item) => ({
       [labelColumn]: item[labelColumn],
-      [dataColumn]: item[dataColumn],
+      [dataColumn]: +item[dataColumn],
     }));
   },
-  groupByLabelsAndCount: (data: any[], labelColumn: string, _: string) => {
+  groupByLabelsAndCount: (
+    data: any[],
+    labelColumn: string,
+    dataColumn: string
+  ) => {
     const groupedData = Object.groupBy(
       data,
       ({ [labelColumn]: label }) => label
     );
     return Object.entries(groupedData).map(([label, values]) => ({
-      label,
-      count: values?.length || 0,
+      [labelColumn]: label,
+      [dataColumn]: values?.length || 0,
     }));
   },
 };
